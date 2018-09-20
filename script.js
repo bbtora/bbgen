3b338f71dc9fcda1f6c9151720a4485fb1017606
bbtora <23010526+bbtora@users.noreply.github.com>
2018-09-20 15:20:03 +0530

    changed to ES6 syntax

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector("#color1");
-var color2 = document.querySelector("#color2");
-var body = document.getElementById("gradient");
-var btn = document.querySelector("button");
+const css = document.querySelector("h3");
+const color1 = document.querySelector("#color1");
+const color2 = document.querySelector("#color2");
+const body = document.getElementById("gradient");
+const btn = document.querySelector("button");
 
 // function to change colors on user input
 function setGradient() {
-    body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
     css.textContent = body.style.background;
 }
 
 // function to generate random hex values
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var rand1 = '#', rand2 = '#';
+    let letters = '0123456789ABCDEF';
+    let rand1 = '#', rand2 = '#';
     for (var i = 0; i < 6; i++) {
       rand1 += letters[Math.floor(Math.random() * 16)];
       rand2 += letters[Math.floor(Math.random() * 16)];
